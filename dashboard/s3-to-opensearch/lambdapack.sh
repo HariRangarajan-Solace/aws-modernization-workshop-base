6fdadae83e5cf2d0ff47df0a0a61ea12e348baff
Hu, Zhiguo <zhigh@amazon.com>
2023-08-06 09:58:38 -0400

    change it to opensearch-py rather than requests

--- dashboard/s3-to-opensearch/lambdapack.sh
+++ dashboard/s3-to-opensearch/lambdapack.sh
@@ -1,4 +1,6 @@
-pip3 install --target ./package requests==2.28.2
+rm -rf package
+#pip3 install --target ./package requests==2.28.2
+pip3 install --target ./package opensearch-py
 pip3 install --target ./package requests_aws4auth
 cd package
 zip -r ../lambda.zip .
--- dashboard/s3-to-opensearch/s3tosearch.py
+++ dashboard/s3-to-opensearch/s3tosearch.py
@@ -1,21 +1,39 @@
 import boto3
-import re
-import requests
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
 from requests_aws4auth import AWS4Auth
 
 
 
 #convert csv string to json
+#convert csv string to json
 def csvToJson(csvStr):
     #split string into array
     csvArr = csvStr.split('|')
-    #create json object
-    jsonObj = {}
-    #add each array element to json object
-    for i in range(len(csvArr)):
-        jsonObj[i] = csvArr[i]
-    #return json object
-    return jsonObj
+
+    if len(csvArr) < 12:
+        print('wrong csv string')
+        return
+    
+    #create json object from csvArr
+    document = {
+        'id': csvArr[0],
+        'ticker': csvArr[1],
+        'name': csvArr[2],
+        'volume': csvArr[3],
+        'price_start': csvArr[4],
+        'price_end': csvArr[5],
+        'change': csvArr[8],
+        'time': csvArr[11]  
+    } 
+
+
+    #convert fileds to numbers
+    document['volume'] = int(document['volume'])
+    document['price_start'] = float(document['price_start'].replace(',','.'))
+    document['price_end'] = float(document['price_end'].replace(',','.'))
+    document['change'] = float(document['change'].replace(',','.'))
+    document['time'] = int(document['time'])    
+    return document
 
 #test function
 def test():
@@ -27,26 +45,29 @@ def test():
 #test()
 
 region = 'us-east-1' # e.g. us-west-1
-service = 'es'
+service = 'aoss'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-host = 'https://zrf9yyk8q2vne4ae5cab.us-east-1.aoss.amazonaws.com' # the OpenSearch Service domain, e.g. https://search-mydomain.us-west-1.es.amazonaws.com
-index = 'lambda-s3-index'
+s3 = boto3.client('s3')
+
+host = 'zrf9yyk8q2vne4ae5cab.us-east-1.aoss.amazonaws.com' # the OpenSearch Service domain, e.g. https://search-mydomain.us-west-1.es.amazonaws.com
+index = 'solace-index'
 datatype = '_doc'
 url = host + '/' + index + '/' + datatype
 
 headers = { "Content-Type": "application/json" }
 
-s3 = boto3.client('s3')
 
 
-
-
-# Regular expressions used to parse some simple log lines
-#ip_pattern = re.compile('(\d+\.\d+\.\d+\.\d+)')
-#time_pattern = re.compile('\[(\d+\/\w\w\w\/\d\d\d\d:\d\d:\d\d:\d\d\s-\d\d\d\d)\]')
-#message_pattern = re.compile('\"(.+)\"')
+client = OpenSearch(
+    hosts = [{'host': host, 'port': 443}],
+    http_auth = awsauth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection,
+    pool_maxsize = 20
+)
 
 
 
@@ -64,10 +85,16 @@ def handler(event, context):
         body = obj['Body'].read()
         lines = body.splitlines()
 
-        # Match the regular expressions to each line and index the JSON
+
         for line in lines:
             line = line.decode("utf-8")
             document=csvToJson(line)
             print(document)
-            r = requests.post(url, auth=awsauth, json=document, headers=headers)
-            print(r.text)
+            # r = requests.post(url, auth=awsauth, json=document, headers=headers)
+            r = client.index(
+                index = 'solace-index',
+                body = document,
+
+               # refresh = True
+                )
+            print(r)
